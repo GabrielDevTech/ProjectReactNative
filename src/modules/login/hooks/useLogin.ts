9ca2f8308e6aa8e85148959897ceb3376468ae4d
Gabriel Leandro <mlkgalero@hotmail.com>
2024-10-30 20:46:45 -0300

    feat: validate email and password in login screen

---
+++ src/modules/login/hooks/useLogin.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
+import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
+
+export const useLogin = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const handleOnPress = async () => {
+        setLoading(true);
+        await axios.post('http://192.168.0.104:8080/auth', {
+            email,
+            password,
+        }).catch(() => {
+            setErrorMessage('Email ou senha inválidos.');
+        });
+        setLoading(false);
+        console.log('clicou')
+    }
+
+    const handleOnchangeEmail = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        setEmail(event.nativeEvent.text);
+    };
+
+    const handleOnchangePassword = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        setPassword(event.nativeEvent.text);
+    };
+
+    return {
+        email,
+        password,
+        loading,
+        errorMessage,
+        handleOnPress,
+        handleOnchangeEmail,
+        handleOnchangePassword
+    }
+}
--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { View } from "react-native"
 import { ContainerLogin, ImageLogo } from "../styles/login.style";
 import Input from "../../../shared/components/Input/Input";
 import Button from "../../../shared/components/button/Button";
@@ -7,30 +5,46 @@ import { theme } from "../../../shared/themes/theme";
 
 import logo from '../../../assets/images/logo.png';
 
+import { useLogin } from "../hooks/useLogin";
+import React from "react";
+import { View } from "react-native";
+
 
 
 const Login = () => {
 
-    const handleOnPress = () => {
-        console.log('clicou')
-    }
+    const {
+        email,
+        password,
+        loading,
+        errorMessage,
+        handleOnPress,
+        handleOnchangeEmail,
+        handleOnchangePassword
+    } = useLogin();
     return (
         <View>
-
             <ContainerLogin>
 
                 <ImageLogo resizeMode="contain" source={logo} />
 
                 <Input
+                    value={email}
+                    errorMessage={errorMessage}
+                    onChange={handleOnchangeEmail}
                     margin="0px 0px 16px 0px"
                     title="Email:"
                     placeholder="Digite seu email" />
 
                 <Input
+                    value={password}
+                    onChange={handleOnchangePassword}
                     title="Senha:"
                     placeholder="Digite sua senha"
+                    errorMessage={errorMessage}
                     secureTextEntry />
                 <Button
+                    loading={loading}
                     type={theme.buttons.buttonsTheme.primary}
                     margin='8px'
                     onPress={handleOnPress}
